7733bbec1bff5cbb7c2c391ed1209118f565abda
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-08-11 22:37:49 +0200

    start to play with Either for validation

--- src/bisect.ts
+++ src/bisect.ts
@@ -1,4 +1,5 @@
 import { Split, split } from './arrays'
+import { Either, Left, Right } from 'purify-ts'
 
 export type Version = string
 export type Suspect<T extends object = {}> = { version: Version } & T
@@ -27,7 +28,18 @@ const remainingSuspectsFrom = <T>(parts: Split<T>, result: 'good' | 'bad' | 'ski
   return result === 'bad' ? parts.left : parts.right
 }
 
-const validateInput = <T extends object>(knownGood: Version, knownBad: Version, suspects: Suspect<T>[]) => {
+type ValidatedInput<T extends object> = {
+  lastGood: Suspect<T>
+  firstBad: Suspect<T>
+  candidates: Suspect<T>[]
+}
+type ValidationResult<T extends object> = Either<BisectError, ValidatedInput<T>>
+
+const validateInput = <T extends object>(
+  knownGood: Version,
+  knownBad: Version,
+  suspects: Suspect<T>[],
+): ValidationResult<T> => {
   const { start, end } = suspects.reduce(
     (acc, cur, index) => {
       if (cur.version === knownGood) {
@@ -41,23 +53,23 @@ const validateInput = <T extends object>(knownGood: Version, knownBad: Version, 
     { start: -1, end: -1 },
   )
   if (start === -1) {
-    return 'good version not in suspects'
+    return Left('good version not in suspects')
   }
   if (end === -1) {
-    return 'bad version not in suspects'
+    return Left('bad version not in suspects')
   }
   if (end < start) {
-    return 'bad version before good version'
+    return Left('bad version before good version')
   }
   let lastGood = suspects[start] as Suspect<T>
   let firstBad = suspects[end] as Suspect<T>
   const candidates = suspects.splice(start + 1, end - 1)
 
-  return {
+  return Right({
     lastGood,
     firstBad,
     candidates,
-  }
+  })
 }
 
 export const bisect = async <T extends object>(
@@ -71,26 +83,31 @@ export const bisect = async <T extends object>(
   const suspects = await scene.suspects()
   const validadtionResult = validateInput(knownGood, knownBad, suspects)
 
-  if (typeof validadtionResult === 'string') {
-    return validadtionResult
+  if (validadtionResult.isLeft()) {
+    return validadtionResult.extract()
   }
 
-  let lastGood = validadtionResult.lastGood
-  let firstBad = validadtionResult.firstBad
-  const candidates = validadtionResult.candidates
+  if (validadtionResult.isRight()) {
+    const validatedInput = validadtionResult.extract()
+    let lastGood = validatedInput.lastGood
+    let firstBad = validatedInput.firstBad
+    const candidates = validatedInput.candidates
 
-  let parts = split(candidates)
+    let parts = split(candidates)
 
-  while (parts.center) {
-    const result = await scene.check(parts.center)
-    if (result === 'bad') {
-      firstBad = parts.center
+    while (parts.center) {
+      const result = await scene.check(parts.center)
+      if (result === 'bad') {
+        firstBad = parts.center
+      }
+      if (result === 'good') {
+        lastGood = parts.center
+      }
+      parts = split(remainingSuspectsFrom(parts, result))
     }
-    if (result === 'good') {
-      lastGood = parts.center
-    }
-    parts = split(remainingSuspectsFrom(parts, result))
+
+    return { lastGood, firstBad }
   }
 
-  return { lastGood, firstBad }
+  throw new Error('should never happen')
 }
