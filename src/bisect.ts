10bfca3824f8aa0d3ac59cf8204829781827a782
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-08-22 22:34:26 +0200

    sort for reading order

--- src/bisect.ts
+++ src/bisect.ts
@@ -22,40 +22,40 @@ type BisectError =
 type BisectOutcome<T extends object = {}> = { lastGood: Suspect<T>; firstBad: Suspect<T> }
 type BisectResult<T extends object = {}> = BisectOutcome<T> | BisectError
 
-const remainingSuspectsFrom = <T>(parts: Split<T>, result: 'good' | 'bad' | 'skip') => {
-  if (result === 'skip') {
-    return [...parts.left, ...parts.right]
+export const bisect = async <T extends object>(
+  knownGood: Version,
+  knownBad: Version,
+  scene: Scene<T>,
+): Promise<BisectResult> => {
+  if (knownGood === knownBad) {
+    return 'knownGood and knowBad are the same'
   }
-  return result === 'bad' ? parts.left : parts.right
-}
+  const suspects = await scene.suspects()
+  const validadtionResult = validateInput(knownGood, knownBad, suspects)
 
-type ValidatedInput<T extends object> = {
-  lastGood: Suspect<T>
-  firstBad: Suspect<T>
-  candidates: Suspect<T>[]
-}
-type ValidationResult<T extends object> = Either<BisectError, ValidatedInput<T>>
-
-type StartEnd = { start: number; end: number }
-const includesGoodVersion = (se: StartEnd): Either<BisectError, StartEnd> => {
-  if (se.start === -1) {
-    return left('good version not in suspects')
+  if (isLeft(validadtionResult)) {
+    return validadtionResult.left
   }
-  return right(se)
-}
 
-const includesBadVersion = (se: StartEnd): Either<BisectError, StartEnd> => {
-  if (se.end === -1) {
-    return left('bad version not in suspects')
-  }
-  return right(se)
-}
+  const validatedInput = validadtionResult.right
+  let lastGood = validatedInput.lastGood
+  let firstBad = validatedInput.firstBad
+  const candidates = validatedInput.candidates
 
-const goodBeforeBadVersion = (se: StartEnd): Either<BisectError, StartEnd> => {
-  if (se.end < se.start) {
-    return left('bad version before good version')
+  let parts = split(candidates)
+
+  while (parts.center) {
+    const result = await scene.check(parts.center)
+    if (result === 'bad') {
+      firstBad = parts.center
+    }
+    if (result === 'good') {
+      lastGood = parts.center
+    }
+    parts = split(remainingSuspectsFrom(parts, result))
   }
-  return right(se)
+
+  return { lastGood, firstBad }
 }
 
 const validateInput = <T extends object>(
@@ -93,38 +93,38 @@ const validateInput = <T extends object>(
   )(startEnd)
 }
 
-export const bisect = async <T extends object>(
-  knownGood: Version,
-  knownBad: Version,
-  scene: Scene<T>,
-): Promise<BisectResult> => {
-  if (knownGood === knownBad) {
-    return 'knownGood and knowBad are the same'
+type ValidatedInput<T extends object> = {
+  lastGood: Suspect<T>
+  firstBad: Suspect<T>
+  candidates: Suspect<T>[]
+}
+type ValidationResult<T extends object> = Either<BisectError, ValidatedInput<T>>
+
+type StartEnd = { start: number; end: number }
+const includesGoodVersion = (se: StartEnd): Either<BisectError, StartEnd> => {
+  if (se.start === -1) {
+    return left('good version not in suspects')
   }
-  const suspects = await scene.suspects()
-  const validadtionResult = validateInput(knownGood, knownBad, suspects)
+  return right(se)
+}
 
-  if (isLeft(validadtionResult)) {
-    return validadtionResult.left
+const includesBadVersion = (se: StartEnd): Either<BisectError, StartEnd> => {
+  if (se.end === -1) {
+    return left('bad version not in suspects')
   }
+  return right(se)
+}
 
-  const validatedInput = validadtionResult.right
-  let lastGood = validatedInput.lastGood
-  let firstBad = validatedInput.firstBad
-  const candidates = validatedInput.candidates
-
-  let parts = split(candidates)
-
-  while (parts.center) {
-    const result = await scene.check(parts.center)
-    if (result === 'bad') {
-      firstBad = parts.center
-    }
-    if (result === 'good') {
-      lastGood = parts.center
-    }
-    parts = split(remainingSuspectsFrom(parts, result))
+const goodBeforeBadVersion = (se: StartEnd): Either<BisectError, StartEnd> => {
+  if (se.end < se.start) {
+    return left('bad version before good version')
   }
+  return right(se)
+}
 
-  return { lastGood, firstBad }
+const remainingSuspectsFrom = <T>(parts: Split<T>, result: 'good' | 'bad' | 'skip') => {
+  if (result === 'skip') {
+    return [...parts.left, ...parts.right]
+  }
+  return result === 'bad' ? parts.left : parts.right
 }
